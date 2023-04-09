a75369e67385c455c5378dcd29b5dfe57676263f
Amandeep Kalakar <amankumaradk@gmail.com>
2023-04-09 21:10:54 +0530

    code updated

---
+++ src/utils/fetchData.jsx
@@ -0,0 +1,14 @@
+export const exerciseOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+    "X-RapidAPI-Key": "4be0d4b77bmshf08173011add6ffp1676b1jsnfed889428788",
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const response = await fetch(url, options);
+  const data = await response.json();
+
+  return data;
+};
